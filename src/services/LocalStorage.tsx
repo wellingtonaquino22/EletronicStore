277865fe945a877f4f35f43c973f08fc328da964
waquino <welaquino202122@gmail.com>
2023-04-02 17:20:43 -0300

    fix:added handling for null type | string in localstorage

--- src/services/LocalStorage.tsx
+++ src/services/LocalStorage.tsx
@@ -3,5 +3,16 @@ export const setItem = (key: any, value: any) =>{
 }
 
 export const getItem = (key: string) => {
-   return JSON.parse(localStorage.getItem(key));
-}
+    try {
+      const value = localStorage.getItem(key);
+      if (value === null) {
+        return null;
+      }
+      return JSON.parse(value);
+    } catch (error) {
+      console.error(`Error parsing JSON from localStorage for key "${key}":`);
+      return null;
+    }
+  }
+  
+ 
